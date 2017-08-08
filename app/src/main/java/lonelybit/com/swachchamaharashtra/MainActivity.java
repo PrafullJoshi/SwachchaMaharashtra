ca1db527e6ce2358b3b8c15cac96221ae26055fa
PrafullJoshi <prafull.joshi@gmail.com>
2017-08-08 23:59:47 +0530

    Image feature started

--- app/src/main/java/lonelybit/com/swachchamaharashtra/MainActivity.java
+++ app/src/main/java/lonelybit/com/swachchamaharashtra/MainActivity.java
@@ -1,7 +1,10 @@
 package lonelybit.com.swachchamaharashtra;
 
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.webkit.ValueCallback;
+import android.webkit.WebChromeClient;
 import android.webkit.WebSettings;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
@@ -15,14 +18,18 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         webview = (WebView) findViewById(R.id.activity_main_webview);
-        webview.setWebViewClient(new WebViewClient());
+        // webview.setWebViewClient(new WebViewClient());
 
         // Enable JS
         WebSettings webSettings = webview.getSettings();
         webSettings.setJavaScriptEnabled(true);
+        webSettings.setAllowFileAccess(true);
+
 
         // Stop local links and redirects from opening in browser instead of WebView
         webview.setWebViewClient(new MyAppWebViewClient());
+
+        webview.setWebChromeClient(new MyWebChromeClient());
 
         // Use local resource
         webview.loadUrl("file:///android_asset/swachcha_maharashtra/index.html");
@@ -33,12 +40,22 @@ public class MainActivity extends AppCompatActivity {
         webview.loadUrl("file:///android_asset/swachcha_maharashtra/index.html");
     }
 
+
     private class MyAppWebViewClient extends WebViewClient {
         @Override
         public boolean shouldOverrideUrlLoading(WebView view, String url) {
             // return super.shouldOverrideUrlLoading(view, url);
             view.loadUrl(url);
             return true;
+        }
+
+
+    }
+
+    private class MyWebChromeClient extends WebChromeClient {
+        @Override
+        public boolean onShowFileChooser(WebView webView, ValueCallback<Uri[]> filePathCallback, FileChooserParams fileChooserParams) {
+            return super.onShowFileChooser(webView, filePathCallback, fileChooserParams);
         }
     }
 }
